6efcd110f6e56b40b11ac8d0e22e8e46e4d90a7a
alanrodgz <alanrodriguez102347@gmail.com>
2020-11-30 19:44:30 -0800

    Walk

---
+++ mainApp/templates/mainApp/main.js
@@ -0,0 +1,15 @@
+const intro = introJs();
+
+intro.setOption() = ({
+    steps: [
+        {
+            intro: 'Welcome to our new website'
+        },
+        {
+            element: '#step-one',
+            intro: 'Please read this!'
+        }
+    ]
+})
+
+intro.start();
